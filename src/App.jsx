b9830a568f40bc4b908e0ef2c309506b640f00e1
ParthPanchal26 <parthpanchal2625@gmail.com>
2025-02-02 20:29:37 +0530

    Connected trending movie appwrite database

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Search from './components/Search'
 import Loader from './components/Loader';
 import MovieCard from './components/MovieCard';
 import { useDebounce } from 'react-use';
+import {updateSearchCount} from './appwrite'
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -51,6 +52,10 @@ function App() {
       }
 
       setMovieList(data.results || []);
+
+      if(query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
 
     } catch (error) {
       console.log(`${error}`);
